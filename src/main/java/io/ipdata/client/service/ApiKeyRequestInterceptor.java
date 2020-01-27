7e334f53c70531b38e31ed29f1069d45346eebb0
yassine <yechabbi@gmail.com>
2020-01-27 19:17:39 +0100

    fixed dangling new line in VERSION file

--- src/main/java/io/ipdata/client/service/ApiKeyRequestInterceptor.java
+++ src/main/java/io/ipdata/client/service/ApiKeyRequestInterceptor.java
@@ -12,17 +12,19 @@ class ApiKeyRequestInterceptor implements RequestInterceptor {
   private static final String API_KEY_PARAM = "api-key";
   private static final String ACCEPT_HEADER = "Accept";
   private static final String JSON_CONTENT  = "application/json";
-  private static final String API_CLIENT_HEADER = "x-client";
-  private static String API_CLIENT_VALUE;
+  private static final String API_CLIENT_HEADER = "User-Agent";
+  private static final String API_CLIENT_VALUE;
   static {
+    String version;
     try {
-      String version = CharStreams.toString(new InputStreamReader(ApiKeyRequestInterceptor.class
-                          .getResourceAsStream("/VERSION")));
-      API_CLIENT_VALUE  = String.format("io.ipdata.client.java.%s", version);
+      version = CharStreams.toString(new InputStreamReader(ApiKeyRequestInterceptor.class
+                          .getResourceAsStream("/VERSION"))).replaceAll("\\n","");
+      version  = String.format("io.ipdata.client.java.%s", version);
     }catch (Exception e){
       e.printStackTrace();
-      API_CLIENT_VALUE  = "io.ipdata.client.java.UNKNOWN";
+      version  = "io.ipdata.client.java.UNKNOWN";
     }
+    API_CLIENT_VALUE = version;
   }
   private final String key;
 
@@ -30,7 +32,7 @@ class ApiKeyRequestInterceptor implements RequestInterceptor {
   public void apply(RequestTemplate template) {
     template.query(API_KEY_PARAM, key);
     template.header(ACCEPT_HEADER, JSON_CONTENT);
-    //template.header(API_CLIENT_HEADER, API_CLIENT_VALUE); TODO: the api doesn't seem to accept any header
+    template.header(API_CLIENT_HEADER, API_CLIENT_VALUE);
   }
 
 }
