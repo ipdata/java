27f7ec46e4075acc8d84d4db8d56e8ac7959b5e5
yassine <yechabbi@gmail.com>
2020-01-27 16:25:05 +0100

    added a runnable demo

--- src/test/java/io/ipdata/client/IpdataTest.java
+++ src/test/java/io/ipdata/client/IpdataTest.java
@@ -1,26 +1,64 @@
 package io.ipdata.client;
 
+import static com.fasterxml.jackson.databind.PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.ipdata.client.service.IpdataField;
 import io.ipdata.client.service.IpdataService;
 import java.net.URL;
-import java.util.Arrays;
 import lombok.SneakyThrows;
-import org.junit.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
 
+@Slf4j
 public class IpdataTest {
 
   private static final String KEY = System.getenv("IPDATACO_KEY");
 
-  @Test
   @SneakyThrows
-  public void test() {
+  public static void main(String[] args) {
+
     URL url = new URL("https://api.ipdata.co");
 
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.setPropertyNamingStrategy(CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
     IpdataService ipdataService = Ipdata.builder()
       .url(url)
       .withDefaultCache()
       .key(KEY)
       .get();
-    System.out.println(ipdataService.bulkIpdata(Arrays.asList("104.238.59.4")));
+
+    LogManager.resetConfiguration();
+    LogManager.getLogger("io").setLevel(Level.OFF);
+    System.out.println("##################### Asking for a full response #############################");
+    long duration = System.currentTimeMillis();
+    String result = mapper.writeValueAsString(ipdataService.ipdata(args[0]));
+    duration = System.currentTimeMillis() - duration;
+    System.out.println(String.format("Got response : %s \nLatency: %s milliseconds", result, duration));
+    System.out.println("################### Asking for ASN field ########################");
+    duration = System.currentTimeMillis();
+    result = mapper.writeValueAsString(ipdataService.asn(args[0]));
+    duration = System.currentTimeMillis() - duration;
+    System.out.println(String.format("Got response : %s \nLatency: %s milliseconds", result, duration));
+    System.out.println("################## Asking for ip, city, and currency field ##########################");
+    duration = System.currentTimeMillis();
+    result = mapper.writeValueAsString(ipdataService.getFields(args[0], IpdataField.IP, IpdataField.CITY, IpdataField.CURRENCY));
+    duration = System.currentTimeMillis() - duration;
+    System.out.println(String.format("Got response : %s \nLatency: %s milliseconds", result, duration));
+    System.out.println("################## Asking for full response that should hit the cache ##########################");
+    duration = System.currentTimeMillis();
+    result = mapper.writeValueAsString(ipdataService.ipdata(args[0]));
+    duration = System.currentTimeMillis() - duration;
+    System.out.println(String.format("Got response : %s \nLatency: %s milliseconds", result, duration));
+
   }
 
 }
